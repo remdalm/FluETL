38252b19328f70819bd5f3e2e74a3cb41552984f
Remi d'Almeida <remi@geta.dev>
2023-08-09 14:05:13 +0300

    add test for converter order

--- src/Infrastructure/converters.rs
+++ src/Infrastructure/converters.rs
@@ -21,3 +21,40 @@ impl From<CsvOrderDTO> for Result<Order, DomainError> {
 pub fn convert_to_orders(dtos: Vec<CsvOrderDTO>) -> Vec<Result<Order, DomainError>> {
     dtos.into_iter().map(|dto| dto.into()).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::fixtures::{csv_order_dto_fixtures, order_fixtures};
+
+    #[test]
+    fn test_convert_to_orders() {
+        let dto_fixtures = csv_order_dto_fixtures();
+        let results: Vec<Result<Order, DomainError>> = convert_to_orders(dto_fixtures.to_vec());
+
+        let order_fixtures = order_fixtures();
+
+        assert!(results[0].is_ok(), "Expected successful conversion");
+        assert_eq!(results[0].as_ref().unwrap(), &order_fixtures[0]);
+
+        assert!(results[1].is_ok(), "Expected successful conversion");
+        assert_eq!(results[1].as_ref().unwrap(), &order_fixtures[1]);
+    }
+
+    #[test]
+    fn test_convert_to_orders_with_errors() {
+        // Simulate a CsvOrderDTO with invalid data for testing validation error
+        let mut dto_fixtures = csv_order_dto_fixtures();
+        dto_fixtures[0].completion = "101".to_string();
+
+        let results: Vec<Result<Order, DomainError>> = convert_to_orders(dto_fixtures.to_vec());
+
+        assert!(
+            results[0].as_ref().is_err_and(|e| match e {
+                DomainError::ValidationError(_) => true,
+                _ => false,
+            }),
+            "Expected Domain Validation Error"
+        );
+    }
+}
